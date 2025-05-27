522ca1ace2ecc6497d10e73cb2fb5e3ccbee054e
ohmoonjeong2 <ohmoonjeong2@gmail.com>
2025-05-27 16:46:23 +0900

    feature : 단위테스트코드 추가

--- src/test/java/TestA.java
+++ src/test/java/TestA.java
@@ -1,8 +1,11 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestA {
     @Test
     void test1(){
         System.out.println("단위테스트");
+        int expect = 1;
+        Assertions.assertTrue(expect == 1);
     }
 }
